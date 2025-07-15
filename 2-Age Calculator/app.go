5474c02e30e4dfe6f10af6772aa48b3d4fe8dbe0
yasin <23070001083@stu.yasar.edu.tr>
2025-07-15 15:23:31 +0300

    added age calculator

---
+++ 2-Age Calculator/app.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Hello to Age Calculator App")
+	fmt.Print("Please enter your birth year: ")
+	birthyear := 0
+	fmt.Scan(&birthyear)
+	currentTime := time.Now()
+	currentYear := currentTime.Year()
+	age := currentYear - birthyear
+	fmt.Println("Your are", age, "years old")
+}
